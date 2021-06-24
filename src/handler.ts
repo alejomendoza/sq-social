9d712f0c782a22e14a5bceedf49223ccc5f747ed
Alejo Mendoza <alejandro@skyhitzmusic.com>
2021-06-24 12:42:46 -0500

    update params

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,9 @@
 export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+  const { searchParams } = new URL(request.url)
+  const duration = searchParams.get('ms_duration');
+  const reward = searchParams.get('xlm_reward');
+  const quest = searchParams.get('quest');
+  const set = searchParams.get('set');
+  
+  return new Response(`${duration} - ${reward} - ${quest} - ${set}`)
 }
