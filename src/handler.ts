5992a2dfee5c78d596c9082e25703e3b106f3dd6
Jesmar Castillo <jesmar@ucsb.edu>
2021-06-24 12:57:33 -0700

    Implement canvas

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,24 @@
-import { screenshot } from './generateImage';
+import { fabric } from 'fabric';
+
+const canvas = () => {
+  const canvas = new fabric.StaticCanvas(null, {
+    width: 600,
+    height: 315,
+    renderOnAddRemove: false,
+  });
+
+  var text = new fabric.Text('Hello world', {
+    left: 100,
+    top: 100,
+    fill: '#FFF',
+  });
+
+  canvas.add(text);
+  canvas.renderAll();
+
+  const image = canvas.toDataURL().replace(/^data:image\/png;base64,/, '');
+  return image;
+};
 
 export async function handleRequest(request: Request): Promise<Response> {
   const { searchParams } = new URL(request.url);
@@ -7,6 +27,6 @@ export async function handleRequest(request: Request): Promise<Response> {
   const quest = searchParams.get('quest');
   const set = searchParams.get('set');
 
-  const base64String = await screenshot({ set, quest, reward });
+  const base64String = await canvas();
   return new Response(base64String);
 }
